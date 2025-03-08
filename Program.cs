404dd20686d7399c4b4f8266d71995e18379fc7a
kumarrishav7 <kumarrishav7@gmail.com>
2025-03-08 12:43:12 +0000

    undo previous test

--- Program.cs
+++ Program.cs
@@ -17,33 +17,33 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//builder.Services.AddSingleton<IConnection>(provider =>
-//{
-//    var telemetryClient = provider.GetRequiredService<TelemetryClient>(); // Inject TelemetryClient
-//    try
-//    {
-//        var factory = new ConnectionFactory() { HostName = builder.Configuration["RabbitMQ:HostName"] };
-//        return factory.CreateConnection();
-//    }
-//    catch (Exception ex)
-//    {
-//        // Log the exception using Application Insights (via injected TelemetryClient)
-//        telemetryClient.TrackException(ex);
+builder.Services.AddSingleton<IConnection>(provider =>
+{
+    var telemetryClient = provider.GetRequiredService<TelemetryClient>(); // Inject TelemetryClient
+    try
+    {
+        var factory = new ConnectionFactory() { HostName = builder.Configuration["RabbitMQ:HostName"] };
+        return factory.CreateConnection();
+    }
+    catch (Exception ex)
+    {
+        // Log the exception using Application Insights (via injected TelemetryClient)
+        telemetryClient.TrackException(ex);
 
-//        // Log locally using the logger
-//        var logger = provider.GetRequiredService<ILogger<Program>>(); // Get ILogger from DI container
-//        logger.LogError(ex, "RabbitMQ connection failed.");
+        // Log locally using the logger
+        var logger = provider.GetRequiredService<ILogger<Program>>(); // Get ILogger from DI container
+        logger.LogError(ex, "RabbitMQ connection failed.");
 
-//        // Rethrow the exception to fail the application startup
-//        throw new InvalidOperationException("Failed to create RabbitMQ connection.", ex);
-//    }
-//});
+        // Rethrow the exception to fail the application startup
+        throw new InvalidOperationException("Failed to create RabbitMQ connection.", ex);
+    }
+});
 
 builder.Services.AddSingleton<MessageRepository>();
-//builder.Services.AddHostedService<RabbitMqService>();
-//builder.Services.AddHostedService<Consumer2Service>();
-//builder.Services.AddSingleton<IQueueChannel, RabbitMqChannel>();
-//builder.Services.AddSingleton<IMessagePublisher, RabbitMqMessagePublisher>();
+builder.Services.AddHostedService<RabbitMqService>();
+builder.Services.AddHostedService<Consumer2Service>();
+builder.Services.AddSingleton<IQueueChannel, RabbitMqChannel>();
+builder.Services.AddSingleton<IMessagePublisher, RabbitMqMessagePublisher>();
 
 var app = builder.Build();
 
