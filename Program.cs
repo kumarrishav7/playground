16624de78989de23876510498f38101e18bde665
kumarrishav7 <kumarrishav7@gmail.com>
2025-03-08 12:35:35 +0000

    just to test

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.ApplicationInsights;
 using NotificationService;
 using RabbitMQ.Client;
 
@@ -16,28 +17,33 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddSingleton<IConnection>(provider =>
-{
-    var factory = new ConnectionFactory()
-    {
-        HostName = builder.Configuration["RabbitMQ:HostName"],
-    };
+//builder.Services.AddSingleton<IConnection>(provider =>
+//{
+//    var telemetryClient = provider.GetRequiredService<TelemetryClient>(); // Inject TelemetryClient
+//    try
+//    {
+//        var factory = new ConnectionFactory() { HostName = builder.Configuration["RabbitMQ:HostName"] };
+//        return factory.CreateConnection();
+//    }
+//    catch (Exception ex)
+//    {
+//        // Log the exception using Application Insights (via injected TelemetryClient)
+//        telemetryClient.TrackException(ex);
 
-    try
-    {
-        return factory.CreateConnection();
-    }
-    catch (Exception ex)
-    {
-        throw new InvalidOperationException("Could not create RabbitMQ connection.", ex);
-    }
-});
+//        // Log locally using the logger
+//        var logger = provider.GetRequiredService<ILogger<Program>>(); // Get ILogger from DI container
+//        logger.LogError(ex, "RabbitMQ connection failed.");
+
+//        // Rethrow the exception to fail the application startup
+//        throw new InvalidOperationException("Failed to create RabbitMQ connection.", ex);
+//    }
+//});
 
 builder.Services.AddSingleton<MessageRepository>();
-builder.Services.AddHostedService<RabbitMqService>();
-builder.Services.AddHostedService<Consumer2Service>();
-builder.Services.AddSingleton<IQueueChannel, RabbitMqChannel>();
-builder.Services.AddSingleton<IMessagePublisher, RabbitMqMessagePublisher>();
+//builder.Services.AddHostedService<RabbitMqService>();
+//builder.Services.AddHostedService<Consumer2Service>();
+//builder.Services.AddSingleton<IQueueChannel, RabbitMqChannel>();
+//builder.Services.AddSingleton<IMessagePublisher, RabbitMqMessagePublisher>();
 
 var app = builder.Build();
 
