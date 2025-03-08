089f6c6bc0edd0090a1d7f531dd35e56fcb75103
kumarrishav7 <kumarrishav7@gmail.com>
2025-03-08 12:45:40 +0000

    test

--- Program.cs
+++ Program.cs
@@ -47,11 +47,8 @@ builder.Services.AddSingleton<IMessagePublisher, RabbitMqMessagePublisher>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
